28bb8c2343ed73c0128b6428080e37fe8bdd7171
Pablo Cusnir <pabloc@kayhut.com>
2017-11-06 18:08:28 +0200

    Add project files.

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Net.Mail;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var encoder = new VideoEncoder();
+            encoder.RegisterNoficationChannel(new MailNotificationChannel());
+            encoder.RegisterNoficationChannel(new SmsNotificationChannel());
+            encoder.Encode(new Video());
+        }
+    }
+}
